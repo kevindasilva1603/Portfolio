0d8290b0b0529bdc9907151c7a66e4db3b2e9233
kevindasilva1603 <kevinsilva1603@gmail.com>
2023-05-22 15:32:32 +0200

    img hover

--- scroll.js
+++ scroll.js
@@ -2,6 +2,6 @@ let btn = document.querySelector('.arrows')
 let el = document.querySelector('.special')
 
 btn.addEventListener('click', function () {
-    el.scrollIntoView({ block: 'end', behavior: 'smooth' })
     el.scrollIntoView(true)
+    el.scrollIntoView({ behavior: 'smooth' })
 })
