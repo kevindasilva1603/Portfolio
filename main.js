a44cb35c9aaf40d33f0482160e04630e7a8e23d4
kevindasilva1603 <kevinsilva1603@gmail.com>
2023-06-06 16:23:34 +0200

    update title

--- main.js
+++ main.js
@@ -197,45 +197,32 @@ window.addEventListener('DOMContentLoaded', () => {
 
 // end mscursor
 document.addEventListener('DOMContentLoaded', function (event) {
-    // array with texts to type in typewriter
     var dataText = ['Kévin Da Silva.', 'Web Developper.', 'Portfolio.', 'bye!']
 
-    // type one text in the typwriter
-    // keeps calling itself until the text is finished
     function title2(text, i, fnCallback) {
-        // chekc if text isn't finished yet
         if (i < text.length) {
-            // add next character to h1
             document.querySelector('h1').innerHTML =
                 text.substring(0, i + 1) + '<span aria-hidden="true"></span>'
 
-            // wait for a while and call this function again for next character
             setTimeout(function () {
                 title2(text, i + 1, fnCallback)
             }, 100)
-        }
-        // text finished, call callback if there is a callback function
-        else if (typeof fnCallback == 'function') {
-            // call callback after timeout
+        } else if (typeof fnCallback === 'function') {
             setTimeout(fnCallback, 700)
         }
     }
-    // start a typewriter animation for a text in the dataText array
-    function StartTextAnimation(i) {
-        if (typeof dataText[i] == 'undefined') {
+
+    function startTextAnimation(i) {
+        if (typeof dataText[i] === 'undefined') {
             setTimeout(function () {
-                StartTextAnimation(0)
-            }, 20000)
-        }
-        // check if dataText[i] exists
-        if (i < dataText[i].length) {
-            // text exists! start typewriter animation
+                startTextAnimation(0)
+            }, 1000)
+        } else if (i < dataText[i].length) {
             title2(dataText[i], 0, function () {
-                // after callback (and whole text has been animated), start next text
-                StartTextAnimation(i + 1)
+                startTextAnimation(i + 1)
             })
         }
     }
-    // start the text animation
-    StartTextAnimation(0)
+
+    startTextAnimation(0)
 })
