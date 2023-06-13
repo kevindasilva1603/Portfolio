8bfbc0ecd4335e635b9d779e7aed6e28e6b3baf9
kevindasilva1603 <kevinsilva1603@gmail.com>
2023-06-13 17:36:58 +0200

    dark mode

--- scroll.js
+++ scroll.js
@@ -5,3 +5,19 @@ btn.addEventListener('click', function () {
     el.scrollIntoView(true)
     el.scrollIntoView({ behavior: 'smooth' })
 })
+
+function toggleDarkMode() {
+    const body = document.body
+    body.classList.toggle('dark-mode')
+
+    const darkModeIcon = document.getElementById('dark-mode-icon')
+    if (body.classList.contains('dark-mode')) {
+        darkModeIcon.src =
+            'https://www.uplooder.net/img/image/55/7aa9993fc291bc170abea048589896cf/sun.svg'
+        darkModeIcon.alt = 'Icône de mode clair'
+    } else {
+        darkModeIcon.src =
+            'https://www.uplooder.net/img/image/2/addf703a24a12d030968858e0879b11e/moon.svg'
+        darkModeIcon.alt = 'Icône de mode sombre'
+    }
+}
